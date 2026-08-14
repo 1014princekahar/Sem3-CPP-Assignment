b30b83e8a582dc2c8cf2990d1dda4543de871ff1
1014princekahar <prince.kahar.king@gmail.com>
2026-08-14 17:51:58 +0530

    added Q1

---
+++ Unit 2/2.2/Q1.cpp
@@ -0,0 +1,28 @@
+/*
+
+Write a C++ program to demonstrate function overloading by creating a class Calculator with overloaded functions add() to perform addition of two integers, three integers, and two floating-point numbers.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void clearScreen()
+{
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+}
+
+class Calaculator
+{
+    
+};
+
+int main()
+{
+    clearScreen();
+    return 0;
+}
